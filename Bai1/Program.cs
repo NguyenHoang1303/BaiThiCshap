24a9c003f7604abe2e75661160c5dd9fb1a1ec14
HoangNguyen <nguyen111@gmail.com>
2021-10-02 20:32:12 +0700

    add

---
+++ Bai1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cylinder = new Cylinder();
+            cylinder.Radius = 38.64;
+            cylinder.Height = 22.48;
+            Console.WriteLine("Enter the dimensions of the cylinder");
+            Console.WriteLine($"Height: {cylinder.Height}");
+            Console.WriteLine($"Radius: {cylinder.Radius}");
+            cylinder.Process();
+            cylinder.Result();
+        }
+        
+    }
+
+     class Cylinder 
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
+        public double BaseArea { get; set; }
+        public double LateralArea { get; set; }
+        public double TotalArea { get; set; }
+        public double Volume { get; set; }
+        public void Process()
+        {
+            BaseArea = Radius * Radius * Math.PI;
+            LateralArea = 2 * Radius * Height * Math.PI;
+            TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            Volume = Math.PI * Radius * Radius * Height;
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("Cylinder Characteristics:");
+            Console.WriteLine($"Radius: {Radius}, Height: {Height}");
+            Console.WriteLine($"Base: {BaseArea}, Lateral: {LateralArea}, Total: {TotalArea}, Volume: {Volume}");
+        }
+
+        public Cylinder(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+
+        public Cylinder()
+        {
+        }
+    }
+    
+    
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tiger = new Tiger();
+            var lion = new Lion();
+            
+            tiger.SetMe("tiger",100);
+            lion.SetMe("lion",200);
+            
+            tiger.Show();
+            lion.Show();
+        }
+    }
+
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}, Weight: {Weight}");
+        }
+        public abstract void SetMe(string name, double weight);
+    }
+
+    class Tiger : Animal
+    {
+        public override void SetMe(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+    }
+
+    class Lion : Animal
+    {
+        public override void SetMe(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+    }
+}
